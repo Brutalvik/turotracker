ae5f980362ab05211194c7d76d3a531adfc34fa6
vik.kumar <vikram.kumar@gsts.ca>
2023-07-29 20:21:15 -0600

    Added aboslute imports and chakra ui themeing

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+//Chakra UI Theme
+import { ColorModeScript } from "@chakra-ui/react";
+import { config } from "theme";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={config.initialColorMode} />
     <App />
   </React.StrictMode>
 );
