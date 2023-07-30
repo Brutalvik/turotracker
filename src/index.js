33f9e037c0d900313a4d7e1442f07862bba747b3
vik.kumar <vikram.kumar@gsts.ca>
2023-07-29 20:22:48 -0600

    Added redux toolkit as dependency

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+//Redux Toolkit
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
 //Chakra UI Theme
+import { ChakraProvider } from "@chakra-ui/react";
 import { ColorModeScript } from "@chakra-ui/react";
 import { config } from "theme";
 
@@ -11,7 +16,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ColorModeScript initialColorMode={config.initialColorMode} />
-    <App />
+    <Provider store={store}>
+      <ChakraProvider>
+        <ColorModeScript initialColorMode={config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>
 );
